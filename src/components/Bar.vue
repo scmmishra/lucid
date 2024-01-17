d892df43899e95d369610059bcb1e2970489fb41
Shivam Mishra <scm.mymail@gmail.com>
2024-01-17 13:11:55 +0530

    feat: allow basic rendering

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { useGraphControl } from "./provider";
 interface Props {
   value: number;
+  readonly index?: number;
   name?: string;
 }
 
@@ -12,8 +14,24 @@ const props = withDefaults(defineProps<Props>(), {
 const graphContext = useGraphControl("Bar");
 const { measurements } = graphContext;
 const effectiveHeight = measurements.value.height * (props.value / 100);
+
+const xPos = computed(() => {
+  if (props.index === undefined) return 0;
+  return props.index * 30;
+});
+
+const yPos = computed(() => {
+  return measurements.value.height - effectiveHeight;
+});
 </script>
 
 <template>
-  <rect :height="effectiveHeight" width="20" fill="sky" style="opacity: 0.2" />
+  <rect
+    :height="effectiveHeight"
+    width="20"
+    fill="sky"
+    style="opacity: 0.2"
+    :x="xPos"
+    :y="yPos"
+  />
 </template>
--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,38 +1,71 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent, h, computed, VNodeArrayChildren } from "vue";
+import Bar from "./Bar.vue";
 import { useElementBounding, useParentElement } from "@vueuse/core";
 import { useGraphControlProvider } from "./provider";
 import type { GraphControlPayload } from "./provider";
-
 import { useClamp } from "@vueuse/math";
-import { computed } from "vue";
 
-interface Props {
-  height: number;
-  maxWidth?: number;
-}
+export default defineComponent({
+  props: {
+    height: {
+      type: Number,
+      default: 240,
+    },
+    maxWidth: Number,
+  },
+  setup(props, { slots }) {
+    const parent = useParentElement();
+    const { width } = useElementBounding(parent);
+    const effectiveWidth = useClamp(width, 0, props.maxWidth ?? width);
 
-const props = withDefaults(defineProps<Props>(), {
-  height: 240,
+    const measurements = computed(() => ({
+      height: props.height,
+      width: effectiveWidth.value,
+    }));
+
+    const api: GraphControlPayload = {
+      measurements: measurements,
+    };
+
+    useGraphControlProvider(api);
+
+    const barSlots = computed(() => {
+      if (!slots.default) return [];
+
+      const slotChildren = slots.default()[0].children as VNodeArrayChildren;
+
+      if (!slotChildren) return [];
+
+      if (Array.isArray(slotChildren)) {
+        return slotChildren.map((vnode, index) => {
+          if (typeof vnode === "string") return vnode;
+          if (typeof vnode === "number") return vnode;
+          if (typeof vnode === "boolean") return vnode;
+          if (Array.isArray(vnode)) return vnode;
+          if (vnode === null) return vnode;
+          if (vnode === undefined) return vnode;
+          if (vnode.type !== Bar) return vnode;
+
+          return h(vnode.type, {
+            ...vnode.props,
+            index,
+          });
+        });
+      }
+
+      return [];
+    });
+
+    return () =>
+      h(
+        "svg",
+        {
+          height: props.height,
+          width: effectiveWidth.value,
+        },
+        barSlots.value
+      );
+  },
 });
-
-const parent = useParentElement();
-const { width } = useElementBounding(parent);
-const effectiveWidth = useClamp(width, 0, props.maxWidth ?? width);
-
-const measurements = computed(() => ({
-  height: props.height,
-  width: effectiveWidth.value,
-}));
-
-const api: GraphControlPayload = {
-  measurements: measurements,
-};
-
-useGraphControlProvider(api);
 </script>
-
-<template>
-  <svg :height="height" :width="effectiveWidth">
-    <slot />
-  </svg>
-</template>
